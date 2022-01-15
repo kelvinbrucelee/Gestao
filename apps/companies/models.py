99909a6c49515e10b071a85e33c18f25ccad121a
kelvinbrucelee <kelvinbrucelee@hotmail.com>
2022-01-15 02:34:13 -0300

    Add Bootstap

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField('Nome', max_length=100, help_text="Nome da Empresa")
 
+    class Meta:
+        verbose_name_plural = 'EMPRESAS'
+        ordering = ("id",)
+
 
     def __str__(self) -> str:
         return self.name
